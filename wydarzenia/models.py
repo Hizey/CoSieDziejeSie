46a14086a9142923d0febc0c11ed7a781ca87190
Karol Bobryk <karolbobryk@bluerider.softwaregit>
2023-03-21 10:02:23 +0100

    działające modelsy

--- wydarzenia/models.py
+++ wydarzenia/models.py
@@ -13,9 +13,9 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=12, decimal_places=2, blank=True)
-    description = models.TextField(blank=True)
-    date = models.DateTimeField(blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -31,8 +31,6 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
-    date = models.DateTimeField(blank=True)
+    date = models.DateTimeField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
-
