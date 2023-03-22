410f4490a8293d418a71f767a7783657c6776add
Karol Bobryk <karolbobryk@bluerider.softwaregit>
2023-03-22 12:21:21 +0100

    fixed deafult

--- wydarzenia/models.py
+++ wydarzenia/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.name
@@ -12,9 +12,9 @@ class Topic(models.Model):
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='')
     price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True, default='')
     date = models.DateTimeField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
