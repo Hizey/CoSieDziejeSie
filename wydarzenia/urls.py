cbf2565fc43a282d298a99f99a144b170bbb6fd8
Michal Burzynski <michalburzynski@bluerider.software>
2023-03-30 11:26:33 +0200

    poprawa rejestracji oraz przypomnienie hasła

--- wydarzenia/urls.py
+++ wydarzenia/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -13,4 +13,8 @@ urlpatterns = [
     path("delete-room/<int:pk>/", views.delete_room, name="delete-room"),
     path("history/", views.history, name="history"),
     path("profile/<int:pk>/", views.profile, name="profile"),
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
